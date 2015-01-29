6bd9d53bb9f163e18fdd711539eb674fc8dcae3a
nobita2811 <svtxphu@gmail.com>
2015-01-29 10:39:55 +0700

    11h am 29/1/2015 Signed-off-by: nobita2811 <svtxphu@gmail.com>

---
+++ application/views/layout/footer.php
@@ -0,0 +1,27 @@
+
+					<?php $this->load->view('layout/pagination'); ?>
+				</div>
+                                <?php $this->load->view('layout/sidebar'); ?>
+			</div>
+			<div class="row clearfix">
+				<div class="col-md-4 column">
+					 <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br /> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address>
+				</div>
+				<div class="col-md-4 column">
+					 <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br /> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address>
+				</div>
+				<div class="col-md-4 column">
+					 <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br /> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-2 column">
+		</div>
+	</div>        
+</div>
+<div class="footer text-center text-success">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></div>
+<script src="<?= getJs('jquery.min') ?>"></script>
+<script src="<?= getJs('bootstrap.min') ?>"></script>
+<script src="<?= getJs('common') ?>"></script>
+</body>
+</html>
